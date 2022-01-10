16d1668c956e91040b3c2640eb1fd30cbe3876f4
hp2309 <hetal.priyadarshi.23@gmail.com>
2022-01-10 07:54:13 +0530

    Master.py

--- code/neural-ode/examples/master.py
+++ code/neural-ode/examples/master.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     i = 500
     while i <= 5000:
         subprocess.call(" python latent_ode.py --adjoint=True --niters=" + str(i), shell=True)
-        i += 2
+        i += 500
     print("Done")
