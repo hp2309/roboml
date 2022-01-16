ab4c750aa18a0afe5e2885109ebb78322b8751c9
hp2309 <hetal.priyadarshi.23@gmail.com>
2022-01-16 22:55:54 +0530

    update with logger

--- code/neural-ode/examples/master.py
+++ code/neural-ode/examples/master.py
@@ -1,16 +1,27 @@
 import subprocess
-import numpy as np
+import time
 
 # subprocess.call(" python slave.py --a=True --n=10 --s='sad'", shell=True)
 
 if __name__ == '__main__':
-    i = 100
-    while i <= 1000:
+    
+    i = 10
+    while i <= 200:
+        f = open("log.txt", "a")
+        s = time.time()
         subprocess.call(" python latent_ode.py --nsample=" + str(i), shell=True)
-        i += 100
+        e = time.time()
+        f.write("latent_ode adjoint=false runtime={} nsample={}\n".format(e-s, i))
+        f.close()
+        i += 10
     print("Done for adjoint False")
-    i = 100
-    while i <= 1000:
+    i = 10
+    while i <= 200:
+        f = open("log.txt", "a")
+        s = time.time()
         subprocess.call(" python latent_ode.py --adjoint=True --nsample=" + str(i), shell=True)
-        i += 100
+        e = time.time()
+        f.write("latent_ode adjoint=true runtime={} nsample={}\n".format(e-s, i))
+        f.close()
+        i += 10
     print("Done for adjoint True")
